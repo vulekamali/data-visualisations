11d5768a1f932a2381bab262e9251508f37da04f
Adi Eyal <adi@openup.org.za>
2019-10-14 18:26:19 +0200

    Added a number of functions
    - Create a save to png button
    - Various svg alignment functions

--- elements.js
+++ elements.js
@@ -28,6 +28,57 @@ function createMainLabel(container, label, config) {
     return programmeButton;
 }
 
+function createSaveButton(container, width, height, imageWidth, imageHeight) {
+    var containerBBox = container.node().getBBox()
+    var saveButtonContainer = container.append("g")
+        .classed("save-button", true)
+        .on("click", function() {
+            saveSvgAsPng(container.node(), "diagram.png", {
+                backgroundColor : "white",
+                left : -10,
+                width: imageWidth + 20,
+                top: -10,
+                height: imageHeight - 20
+            });
+        })
+
+    var saveButtonRect = saveButtonContainer.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+            .attr("rx", 3)
+            .attr("ry", 3)
+
+    var saveButtonContents = saveButtonContainer.append("g")
+
+    var saveButtonText = saveButtonContents.append("text")
+        .text("Download Image")
+        .attr("text-anchor","left")
+        .each(textBump)
+
+    var saveButtonImageGroup = saveButtonContents
+        .append("g")
+
+    var saveButtonImage = saveButtonImageGroup
+        .append("path")
+            .attr("d", "M459,408V51c0-28.05-22.95-51-51-51H51C22.95,0,0,22.95,0,51v357c0,28.05,22.95,51,51,51h357 C436.05,459,459,436.05,459,408z M140.25,267.75l63.75,76.5l89.25-114.75L408,382.5H51L140.25,267.75z")
+            .attr("transform", "scale(0.03, 0.03)")
+
+    leftMargin(saveButtonImageGroup, saveButtonText, 5)
+
+    centerAlign(saveButtonContainer, saveButtonContents)
+    verticalAlign(saveButtonImageGroup, saveButtonText)
+    verticalAlign(saveButtonRect, saveButtonContents)
+
+        d3.selectAll("button")
+            .on("click", function() {
+                svg = d3.selectAll("svg")
+                    .attr("viewport", "(0, 0 1000, 800)")
+                saveSvgAsPng(svg.node(), "diagram.png");
+            })
+
+    return saveButtonContainer
+}
+
 function createSVG(container, width, height) {
     var svg = container
         .append("svg")
@@ -36,7 +87,6 @@ function createSVG(container, width, height) {
             .attr("xmlns", "http://www.w3.org/2000/svg")
             .attr("preserveAspectRatio", "xMinYMin meet")
             .classed("svg-content-responsive", true)
-            .append("g")
 
     return svg
 }
@@ -65,6 +115,42 @@ function addLinearGradient(container, id, color1, color2, offset1, offset2, opac
         .attr("offset", offset2)
         .attr("stop-color", color2)
         .style("stop-opacity", opacity2)
+}
+
+
+function centerAlign(el1, el2) {
+    var bbox1 = el1.node().getBBox()
+    var bbox2 = el2.node().getBBox()
+    var leftMargin = (bbox1.width - bbox2.width) / 2
+    var currentTransforms = el2.attr("transform") || ""
+
+    el2.attr("transform", "translate(" + leftMargin + ", 0) " + currentTransforms)
+}
+
+function verticalAlign(el1, el2) {
+    var bbox1 = el1.node().getBBox()
+    var bbox2 = el2.node().getBBox()
+    var topMargin = (bbox1.height - bbox2.height) / 2
+    var currentTransforms = el2.attr("transform") || ""
+
+    el2.attr("transform", "translate(0, " + topMargin + ") " + currentTransforms)
+}
+
+function leftMargin(el1, el2, margin) {
+    var bbox1 = el1.node().getBBox()
+    var left = (bbox1.width + margin)
+    var currentTransforms = el2.attr("transform") || ""
+
+    el2.attr("transform", "translate(" + left + ", 0) " + currentTransforms)
+}
+
+function textBump() {
+    var text = d3.select(this) 
+    var bbox = text.node().getBBox()
+    var currentTransforms = text.attr("transform") || ""
+    text
+        .attr("transform", "translate(0, " + bbox.height + ") " + currentTransforms)
+        .attr("dy", "-0.2em")
 }
 
 colorMap = [
