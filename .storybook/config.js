17d2bc6c07eba69202b4951b82d8d97a52974630
Andrey <andrey.zolotin@gmail.com>
2019-11-20 14:27:55 +0100

    fix: attempt to fix the IE build

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,13 @@
+require('weakmap-polyfill');
+import 'es6-shim'
+import 'es5-shim';
+import "core-js";
+import "core-js/stable";
+import "core-js/es/set";
+import "core-js/es/weak-map";
+import "core-js/es/map";
+import "regenerator-runtime/runtime";
+
 import { configure } from '@storybook/html';
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
