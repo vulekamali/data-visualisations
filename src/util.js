83553e4a4be2ddb166099ef17ea4b41f355818e6
JD Bothma <jbothma@gmail.com>
2020-01-31 16:29:45 +0200

    fix narrow humanising and linting issues

--- src/util.js
+++ src/util.js
@@ -1,22 +1,26 @@
 import { format as d3Format } from 'd3-format';
 
-export const humaniseRand = function (x, longForm) {
-  longForm = longForm == undefined ? true : longForm;
-  const suffixBillion = longForm == true ? ' billion' : 'bn';
-  const suffixMillion = longForm == true ? ' million' : 'm';
-  const suffixThousand = longForm == true ? '  thousand' : 'k';
 
-  if (Math.abs(x) >= 1000000000) {
-    return formatRand(x / 1000000000) + suffixBillion;
-  } if (Math.abs(x) >= 1000000) {
-    return formatRand(x / 1000000) + suffixMillion;
-  } if (!longForm && Math.abs(x) >= 100000) {
-    return formatRand(x / 1000) + suffixThousand;
-  }
-  return formatRand(x, 0);
+const formatRand = (x, decimals, randSpace) => {
+  decimals = decimals === undefined ? 1 : decimals; // eslint-disable-line no-param-reassign
+  randSpace = randSpace === undefined ? ' ' : ''; // eslint-disable-line no-param-reassign
+  return `R${randSpace}${d3Format(`,.${decimals}f`)(x)}`;
 };
 
-const formatRand = function (x, decimals) {
-  decimals = decimals == undefined ? 1 : decimals;
-  return `R ${d3Format(`,.${decimals}f`)(x)}`;
+export const humaniseRand = (x, longForm) => { // eslint-disable-line import/prefer-default-export
+  longForm = longForm === undefined ? true : longForm; // eslint-disable-line no-param-reassign
+  const randSpace = longForm ? ' ' : '';
+  const decimals = longForm ? 1 : 0;
+  const suffixBillion = longForm === true ? ' billion' : 'bn';
+  const suffixMillion = longForm === true ? ' million' : 'm';
+  const suffixThousand = longForm === true ? '  thousand' : 'k';
+
+  if (Math.abs(x) >= 1000000000) {
+    return formatRand(x / 1000000000, decimals, randSpace) + suffixBillion;
+  } if (Math.abs(x) >= 1000000) {
+    return formatRand(x / 1000000, decimals, randSpace) + suffixMillion;
+  } if (!longForm && Math.abs(x) >= 100000) {
+    return formatRand(x / 1000, decimals, randSpace) + suffixThousand;
+  }
+  return formatRand(x, 0);
 };
