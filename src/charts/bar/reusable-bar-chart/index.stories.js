fed3e2baf993ab6cdfa5345b1cce56bc9f6cc662
JD Bothma <jbothma@gmail.com>
2020-01-31 15:12:53 +0200

    Fix linting errors that can be fixed automatically

--- src/charts/bar/reusable-bar-chart/index.stories.js
+++ src/charts/bar/reusable-bar-chart/index.stories.js
@@ -1,128 +1,126 @@
 import './stories.styles.css';
-import {select} from 'd3-selection';
-import {reusableBarChart} from './reusable-bar-chart';
+import { select } from 'd3-selection';
+import { reusableBarChart } from './reusable-bar-chart';
 
-export default {title: 'Reusable Bar Chart'};
+export default { title: 'Reusable Bar Chart' };
 
 function generateData() {
-    const items = [];
+  const items = [];
 
-    for (let i = 0; i < 10; i++) {
-        items.push({
-            label: String.fromCharCode(65 + i),
-            value: Math.random() * 100 + 10,
-        });
-    }
+  for (let i = 0; i < 10; i++) {
+    items.push({
+      label: String.fromCharCode(65 + i),
+      value: Math.random() * 100 + 10,
+    });
+  }
 
-    return items;
+  return items;
 }
 
 export const Simple = () => {
-    const container = document.createElement("div");
-    container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-    const myChart = reusableBarChart();
+  const myChart = reusableBarChart();
 
-    select(container)
-        .call(myChart.data(generateData()));
+  select(container)
+    .call(myChart.data(generateData()));
 
-    return container;
+  return container;
 };
 
 export const UpdatingData = () => {
-    const container = document.createElement("div");
-    container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-    const chartContainer = document.createElement("div");
-    chartContainer.classList.add("chartContainer");
+  const chartContainer = document.createElement('div');
+  chartContainer.classList.add('chartContainer');
 
-    const button = document.createElement("button");
-    button.innerHTML = "Update data";
+  const button = document.createElement('button');
+  button.innerHTML = 'Update data';
 
-    container.appendChild(chartContainer);
-    container.appendChild(button);
+  container.appendChild(chartContainer);
+  container.appendChild(button);
 
-    const myChart = reusableBarChart();
-    myChart.height(100);
-    myChart.width(800);
+  const myChart = reusableBarChart();
+  myChart.height(100);
+  myChart.width(800);
 
-    button.addEventListener("click", () => {
-        myChart.data(generateData())
-    });
+  button.addEventListener('click', () => {
+    myChart.data(generateData());
+  });
 
-    select(chartContainer)
-        .call(myChart.data(generateData()));
+  select(chartContainer)
+    .call(myChart.data(generateData()));
 
-    return container;
+  return container;
 };
 
 export const UpdatingColor = () => {
-    const container = document.createElement("div");
-    container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-    const chartContainer = document.createElement("div");
-    chartContainer.classList.add("chartContainer");
+  const chartContainer = document.createElement('div');
+  chartContainer.classList.add('chartContainer');
 
-    const button = document.createElement("button");
-    button.innerHTML = "Update color";
+  const button = document.createElement('button');
+  button.innerHTML = 'Update color';
 
-    container.appendChild(chartContainer);
-    container.appendChild(button);
+  container.appendChild(chartContainer);
+  container.appendChild(button);
 
-    const myChart = reusableBarChart();
-    myChart.height(100);
-    myChart.width(800);
+  const myChart = reusableBarChart();
+  myChart.height(100);
+  myChart.width(800);
 
-    button.addEventListener("click", () => {
-        myChart.colors(['orange', 'red'])
-    });
+  button.addEventListener('click', () => {
+    myChart.colors(['orange', 'red']);
+  });
 
-    select(chartContainer)
-        .call(myChart.data(generateData()));
+  select(chartContainer)
+    .call(myChart.data(generateData()));
 
-    return container;
+  return container;
 };
 
 export const TooltipFormatter = () => {
-    const container = document.createElement("div");
-    container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-    const myChart = reusableBarChart();
-    myChart.height(100);
-    myChart.width(800);
-    myChart.tooltipFormatter((d) => {
-        return `Label: ${d.data.label} </br> Value: ${d.data.value}`;
-    });
+  const myChart = reusableBarChart();
+  myChart.height(100);
+  myChart.width(800);
+  myChart.tooltipFormatter((d) => `Label: ${d.data.label} </br> Value: ${d.data.value}`);
 
-    select(container)
-        .call(myChart.data(generateData()));
+  select(container)
+    .call(myChart.data(generateData()));
 
-    return container;
+  return container;
 };
 
 export const UpdatePadding = () => {
-    const container = document.createElement("div");
-    container.classList.add("container");
+  const container = document.createElement('div');
+  container.classList.add('container');
 
-    const chartContainer = document.createElement("div");
-    chartContainer.classList.add("chartContainer");
+  const chartContainer = document.createElement('div');
+  chartContainer.classList.add('chartContainer');
 
-    const button = document.createElement("button");
-    button.innerHTML = "Update padding";
+  const button = document.createElement('button');
+  button.innerHTML = 'Update padding';
 
-    container.appendChild(chartContainer);
-    container.appendChild(button);
+  container.appendChild(chartContainer);
+  container.appendChild(button);
 
-    const myChart = reusableBarChart();
-    myChart.height(100);
-    myChart.width(800);
+  const myChart = reusableBarChart();
+  myChart.height(100);
+  myChart.width(800);
 
-    select(chartContainer)
-        .call(myChart.data(generateData()));
+  select(chartContainer)
+    .call(myChart.data(generateData()));
 
-    button.addEventListener("click", () => {
-        myChart.padding(10);
-    });
+  button.addEventListener('click', () => {
+    myChart.padding(10);
+  });
 
-    return container;
+  return container;
 };
