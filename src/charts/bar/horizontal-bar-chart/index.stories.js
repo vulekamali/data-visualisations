6582d4f66e67819730b4eb6923469fa4c92a6dcf
JD Bothma <jbothma@gmail.com>
2020-03-18 16:31:00 +0200

    Add grouped and filtered horizontal bar chart story

--- src/charts/bar/horizontal-bar-chart/index.stories.js
+++ src/charts/bar/horizontal-bar-chart/index.stories.js
@@ -31,6 +31,84 @@ export const Simple = () => {
         .data(items)
         .nameKey("label_full")
         .valueKey("value");
-  window.setTimeout(function() { console.log("here"); myChart.reDraw() }, 5000);
+
+  // hack to draw after returning because dev didn't listen to requirements
+  window.setTimeout(function() { console.log("here"); myChart.reDraw(); }, 5000);
+
+  return container;
+};
+
+export const GroupedFiltered = () => {
+  const id = "horizontal-bars-grouped";
+
+  const items = [
+    {
+      "label": "Nelson Mandela Bay",
+      "code": "NMA",
+      "province": "Eastern Cape",
+      "geo_level": "Metropolitain Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/municipality-NMA",
+      "amount": 1123456789,
+    },
+    {
+      "label": "Buffalo City",
+      "code": "BUF",
+      "province": "Eastern Cape",
+      "geo_level": "Metropolitain Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/municipality-NMA",
+      "amount": 3123456789,
+    },
+    {
+      "label": "Sara Baartman",
+      "code": "DC10",
+      "province": "Eastern Cape",
+      "geo_level": "District Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/district-DC10",
+      "amount": 2123456789,
+    },
+    {
+      "label": "Cape Town",
+      "code": "CPT",
+      "province": "Western Cape",
+      "geo_level": "Metropolitain Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/municipality-CPT",
+      "amount": 4123456789,
+    },
+    {
+      "label": "Small cape muni",
+      "code": "WC123",
+      "province": "Western Cape",
+      "geo_level": "Local Municipalities Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/municipality-NMA",
+      "amount": 3123456789,
+    },
+    {
+      "label": "A district",
+      "code": "DC20",
+      "province": "Western Cape",
+      "geo_level": "District Municipalities",
+      "more_url": "https://municipalmoney.gov.za/profiles/district-DC20",
+      "amount": 2123456789,
+    },
+  ];
+
+  const container = document.createElement('div');
+  container.id = id;
+
+  const myChart = new HorizontalBarChart()
+        .select(id)
+        .data(items)
+        .nameKey("label")
+        .valueKey("amount")
+        .groupKey("geo_level")
+        .filterKey("province")
+        .colors(['#6E2195'])
+        .urlKey("more_url")
+        .xAxisUnit('B')
+        .barUnit('M');
+
+  // hack to draw after returning because dev didn't listen to requirements
+  window.setTimeout(function() { console.log("here"); myChart.reDraw(); }, 5000);
+
   return container;
 };
